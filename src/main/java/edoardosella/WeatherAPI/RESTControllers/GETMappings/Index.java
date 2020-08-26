2af9a3222cde9ee6dd915b6ec657d31602d84879
Edoardo Sella <eddy.sella@gmail.com>
2020-08-26 16:45:02 +0200

    Typo in Index message

--- src/main/java/edoardosella/WeatherAPI/RESTControllers/GETMappings/Index.java
+++ src/main/java/edoardosella/WeatherAPI/RESTControllers/GETMappings/Index.java
@@ -16,7 +16,7 @@ public class Index {
                         "date: the date of travel (i.e. dd-MM-yyyy), must be within the next 5 days\n" +
                         "cities: one/multiple city ids separated by '&' \n" +
                         "apikey: an API key from :  https://developer.accuweather.com/packages (limited trial should suffice) \n" +
-                        "2) /lastFive : Returns the JSON response for each of the last 5 routes which were queried";
+                        "2) /lastFiveQueries : Returns the JSON response for each of the last 5 routes which were queried";
 
         return ResponseEntity.ok(output);
     }
