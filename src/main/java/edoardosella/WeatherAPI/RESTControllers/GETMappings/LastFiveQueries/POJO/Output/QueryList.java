796cc54ab76f748cd05ec4bd018236946a396dc6
Edoardo Sella <eddy.sella@gmail.com>
2020-08-26 14:18:22 +0200

    Forgot to include this in the LastFiveQueriesResponseProcessor commit. Custom POJO for JSON output

---
+++ src/main/java/edoardosella/WeatherAPI/RESTControllers/GETMappings/LastFiveQueries/POJO/Output/QueryList.java
@@ -0,0 +1,29 @@
+package edoardosella.WeatherAPI.RESTControllers.GETMappings.LastFiveQueries.POJO.Output;
+
+import edoardosella.WeatherAPI.RESTControllers.GETMappings.WeatherRoute.POJO.Output.Route;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QueryList {
+
+    List<String> routeList;
+
+    public QueryList(){ this.routeList = new ArrayList<>(); }
+
+    public QueryList(List<String> routeList){
+        this.routeList = routeList;
+    }
+
+    public void addRoute(String route){
+        this.routeList.add(route);
+    }
+
+    public List<String> getRouteList() {
+        return routeList;
+    }
+
+    public void setRouteList(List<String> routeList) {
+        this.routeList = routeList;
+    }
+}
